a190321b85386ea43b3bb4a31d04f3a14d664416
Louis Chavane <louis.chavane@gmail.com>
2016-06-06 20:12:24 +0200

    remove test on mailchimp form

--- test/controllers/contacts_controller_test.rb
+++ test/controllers/contacts_controller_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 class ContactsControllerTest < ActionDispatch::IntegrationTest
 
-  test 'should get create' do
-    post contacts_url, params: {email: "test@test.com"}
-    assert_response :success
-  end
+  # test 'should get create' do
+  #   post contacts_url, params: {email: "test@test.com"}
+  #   assert_response :success
+  # end
 
 end
