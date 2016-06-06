d5a4c33c95fc1bafa2507b17e8a6bb27db534237
Louis Chavane <louis.chavane@gmail.com>
2016-06-06 19:09:01 +0200

    connect form to mailchimp

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,14 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(function() {
+    $('#subscribe-form')
+        .bind('ajax:success', function(e, data, status, xhr) {
+            $(".form-control").addClass("success");
+        })
+        .bind('ajax:error', function(e, xhr, status, error) {
+           $(".form-control").addClass("error");
+           $(".form-error").text()
+        });
+})
