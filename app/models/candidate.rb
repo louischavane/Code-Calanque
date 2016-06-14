a6cad8e50da6965576f85057b422f3e795ab0d53
Louis Chavane <louis.chavane@gmail.com>
2016-06-14 17:28:20 +0200

    add validation on candidate

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,6 @@
 class Candidate < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 end
