dc27231d8e4fa59bb6bfec5b202fdebdad89cfd1
Louis Chavane <louis.chavane@gmail.com>
2016-06-15 12:26:50 +0200

    add program details and page

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   get 'le-programme', to:'pages#program', as: 'program'
-  get 'inscription', to:'pages#inscription', as: 'inscription'
 
   resources "contacts", only: [:new, :create]
 
